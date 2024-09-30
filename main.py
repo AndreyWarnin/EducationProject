daeda1d706d93b94d5c230a34db460ae04e97fc8
AndreyWarnin <archyrysss@yahoo.com>
2024-09-30 15:30:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# 1st program
+
+print(9 ** 0.5 * 5)
+
+# 2nd program
+
+b1 = 9.99
+b2 = 9.98
+b3 = 1000
+b4 = 1000.1
+print(b1 > b2 and b3 != b4)
+
+# 3d program
+
+a = 2
+result1 = a * a + a
+result2 = a * (a + a)
+print(result1)
+print(result2)
+print(result1 == result2)
+
+# 4th program
+
+a = '123.456'
+b = float(a)
+c = b * 10
+d = int(c) % 10
+print(d)
+
