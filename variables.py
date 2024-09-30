8caa93d0e7769cf8cc964b4e0b6ae8eb7ca93be5
AndreyWarnin <archyrysss@yahoo.com>
2024-09-30 17:31:42 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+home_work_amount = 12
+hour_amount = 1.5
+course_name = "python"
+hours_per_home_work = hour_amount / home_work_amount
+print("Курс: ", course_name, ", всего задач: ", home_work_amount, ", среднее время выполнения: ", hours_per_home_work)
