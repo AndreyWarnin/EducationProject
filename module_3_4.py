0eb1aa207a81a835ce86087571d020e2fb15d15a
AndreyWarnin <archyrysss@yahoo.com>
2024-11-01 18:09:46 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,10 +1,18 @@
-def single_root_words(root_word, *other_words):
-    for word in other_words:
-        if word.lower == root_word.lower:
-            return other_words
-    return None
 
-root_word = 'sky'
-other_words = ['wood', 'staff', 'sky', 'angle']
-same_words = single_root_words(root_word, *other_words)
-print(same_words)
+def single_root_words(root_word, *other_words):
+    same_words = []
+    lower_root_word = root_word.lower()
+
+    for other_word in other_words:
+        lower_other_word = other_word.lower()
+        if lower_other_word.__contains__(lower_root_word):
+            same_words.append(lower_other_word)
+        elif lower_root_word.__contains__(lower_other_word):
+            same_words.append(lower_other_word)
+
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
